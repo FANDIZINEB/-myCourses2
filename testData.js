033703539c857feace0ad2249a23f2ff92c18043
FANDIZINEB <fandizineb37@gmail.com>
2026-05-16 23:30:37 +0200

    Add screenshots, update testData CDN images

--- testData.js
+++ testData.js
@@ -5,7 +5,7 @@ const testData = [
     description: "Apprenez React Native de zéro",
     price: 29.99,
     image: "https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg",
-    instructor: "Jean Dupont",
+    instructor: "ZINEB FANDI",
   },
   {
     id: 2,
@@ -13,15 +13,15 @@ const testData = [
     description: "Maîtrisez Redux dans vos apps",
     price: 39.99,
     image: "https://cdn.pixabay.com/photo/2016/11/30/20/58/programming-1873854_1280.png",
-    instructor: "Marie Martin",
+    instructor: "ZINEB FANDI",
   },
   {
     id: 3,
-    title: "JavaScript Avancé",
-    description: "ES6, async/await, closures...",
+    title: "Big Data ",
+    description: "Comprenez les concepts avancés du Big Data",
     price: 49.99,
     image: "https://cdn.pixabay.com/photo/2015/12/04/14/05/code-1076536_1280.jpg",
-    instructor: "Paul Bernard",
+    instructor: "ZINEB FANDI",
   },
 ];
 
